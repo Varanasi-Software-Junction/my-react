15bbb2c8ce8e12a77e7a35b1114da527d968cdf0
champaksworld <champaksworld@users.noreply.github.com>
2021-09-16 09:54:04 +0530

    Using the setState hook

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import VsjItem from './components/VsjItem';
 import VsjWrapper from './components/VsjWrapper';
+import VsjCounter from './components/VsjCounter';
 function App() {
   return (
     <div className="App">
+	<VsjCounter/>
      <h1>Welcome to VSJ React</h1>
 	 <VsjItem message="Hello Varanasi Software Junction"></VsjItem>
 	 <VsjItem message="Learn React Here"></VsjItem>
---
+++ src/components/VsjCounter.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import './vsjCounter.css'
+const  VsjCounter = (props) =>
+{
+	const [count, setCount] = useState(0);
+	const [a,b, setAdd] = useState(0,0);
+	return <p>
+	
+	<h2>Count = {count}</h2>
+	<button onClick={() => setCount(count + 1)}>
+        Counter
+      </button>
+	  
+	</p>;
+}
+export default VsjCounter;
