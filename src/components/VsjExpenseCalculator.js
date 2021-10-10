505194ec869b0020c9a29cf9c7b97d68a13896aa
Champak's World <champaksworld@gmail.com>
2021-10-10 16:34:13 +0530

    Added one event handler
    Added one event handler and printed to the console log

--- src/components/VsjExpenseCalculator.js
+++ src/components/VsjExpenseCalculator.js
@@ -2,21 +2,32 @@ import React, { useState } from 'react';
 
 const  VsjExpenseCalculator = (props) =>
 {
-	const [count, setCount] = useState(0);
-	const [num1, setAdd1] = useState(0);
-	const [num2, setAdd2] = useState(0);
+	const [
+	expenseData, setExpense] = useState({
+		item:'',
+		amount:0,
+		tdate:''
+		
+		/* Use n object instead of two useState*/
+	});
+	const itemChangeHandler=(event)=>
+	{
+		setExpense(event.target.value);
+		console.log(event.target.value);
+	};
+	
 	return  <form>
   <div class="form-group">
     <label for="item">Item</label>
-    <input type="email" class="form-control" placeholder="Enter Item" id="item"/>
+    <input type="text" class="form-control" onChange={itemChangeHandler} placeholder="Enter Item" id="item"/>
   </div>
   <div class="form-group">
     <label for="amount">Amount</label>
     <input type="number" class="form-control" placeholder="Enter Amount" id="amount"/>
   </div>
   <div class="form-group">
-    <label for="date">Date</label>
-    <input type="date" class="form-control" placeholder="Enter Date" id="date"/>
+    <label for="tdate">Date</label>
+    <input type="date" class="form-control" placeholder="Enter Date" id="tdate"/>
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>;
