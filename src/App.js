77e6a915c23432de3b1784b38cc67b6a46c0f12f
champaksworldcreate <champaksworld@gmail.com>
2023-09-02 08:17:10 +0530

    React Revisited

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import VsjExpenseCalculator from './components/VsjExpenseCalculator';
-import VsjExpense from './components/VsjExpense';
-import VsjWeather from './components/VsjWeather';
 
 function App() {
   return (
-   <div  class="container pt-5 bg-primary text-white text-center" id="root">
-   <div class="row">
-  <div class="col-sm-1"></div>
-  <div class="col-sm-10 bg-white">
-  {/*<VsjExpenseCalculator></VsjExpenseCalculator>*/}
-  
-  </div>
-  <div class="col-sm-1"></div>  
-   </div>
-   <br/>
-{/*<VsjExpense></VsjExpense>}*/}
-<VsjWeather></VsjWeather>
-   
-   
-   </div>
+    <div>
+      <h1>Hello from Varanasi Software Junction</h1>
+    </div>
   );
 }
 
