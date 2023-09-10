23ffffcf68447a1643ecb2ccfeb006e63dcb6690
champaksworldcreate <champaksworld@gmail.com>
2023-09-10 18:50:40 +0530

    Simple useState counter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function Counter() {
+  // Declare a state variable called "count" with an initial value of 0
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    // Update the "count" state by 1
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    // Update the "count" state by -1
+    setCount(count - 1);
+  };
+
   return (
     <div>
-      <h1>Hi from Varanasi Software Junction</h1>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
 
-export default App;
+export default Counter;
